787a29527caa9ef49593ae03fea725b476af4a0e
Sebastian Iulian Poiana <sebastianpoiana2015@gmail.com>
2020-03-18 18:57:11 +0000

    Update main.js
    -I have made the text field display a red error when the user either introduces a wrong variable or doesn't introduce anything within the bracket.
    -I have also made the shaking animation for the form to go well with the error.

--- js/main.js
+++ js/main.js
@@ -25,7 +25,12 @@ const inputProgress = document.querySelector('#input-progress');
 const progress = document.querySelector('#progress-bar');
 
 // Events
+
+// Get Question on DOM Load
 document.addEventListener('DOMContentLoaded', getQuestion);
+
+// Next Button Click
+nextBtn.addEventListener('click', validate);
 
 // Functions
 
@@ -47,5 +52,51 @@ function getQuestion() {
   progress.style.width = (position * 100) / questions.length + '%';
 
   // Add User Icon OR Back Arrow Depending on Question
+  // Reason why the User Icon did not appear is because of the "import { types } from 'node-sass';", which I have deleted
   prevBtn.className = position ? 'fa fa-arrow-left' : 'fa fa-user';
+
+  showQuestion();
 }
+
+// Display Question To User
+function showQuestion() {
+  inputGroup.style.opacity = 1;
+  inputProgress.style.transition = '';
+  inputProgress.style.width = '100%';
+}
+
+// Hide Question From User
+function hideQuestion() {
+  inputGroup.style.opacity = 0;
+  inputLabel.style.marginLeft = 0;
+  inputProgress.style.width = 0;
+  inputProgress.style.transition = 'none';
+  inputGroup.style.border = null;
+}
+
+// Transform to Create Shake Motion
+function transform(x, y) {
+  formBox.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+// Validate Field
+function validate() {
+  // Make sure that the pattern matches if there is one
+  if (!inputField.value.match(questions[position].pattern || /.+/)) {
+    inputFail();
+  } else {
+    inputPass();
+  }
+}
+
+// Field Input Fail
+function inputFail() {
+  formBox.className = 'error';
+  // Repeat Shake Motion - Set i to number of shakes
+  for (let i = 0; i < 6; i++) {
+    setTimeout(transform, shakeTime * i, ((i % 2) * 2 - 1) * 20, 0);
+  }
+}
+
+// Field Input Pass
+function inputPass() {}
